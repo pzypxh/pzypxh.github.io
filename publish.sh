d78a0ca1ac74cbca1de65e04cc8ef4c1728c3f56
pzypxh <pzypxh@gmail.com>
2021-06-02 15:22:48 +0800

    build at 2021年 6月 2日 星期三 15时22分48秒 CST

--- publish.sh
+++ publish.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 echo "git add ."
-git add .
+git add -A
 
 #echo "git commit"
 #comment="commit new code"
 #read comment
 #git commit -m "$comment"
 
-git commit -m "build at $(date)"
+git commit -am "build at $(date)"
 
-echo "git fetch origin master"
-git fetch origin master
+#echo "git fetch origin master"
+#git fetch origin master
 
-echo "git merge origin/master"
-git merge origin/master
+#echo "git merge origin/master"
+#git merge origin/master
 
 echo "git push origin master:master"
-git push origin master:master
+git push
